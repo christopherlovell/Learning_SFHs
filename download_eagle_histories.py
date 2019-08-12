3f2794403f56e024385e854ffe1d91f5e0d0dfae
christopherlovell <chrisl1991@hotmail.co.uk>
2019-08-12 16:02:16 +0100

    download eagle script added

---
+++ download_eagle_histories.py
@@ -0,0 +1,162 @@
+## Must be run on COSMA
+
+import eagle as E
+import numpy as np
+
+from scipy.spatial.distance import cdist
+
+# import sys
+# sys.path.append('/cosma5/data/dp004/dc-love2/sph2sed')
+# from sph2sed.model import sed
+# sp = sed()
+
+directory = '/cosma5/data/Eagle/ScienceRuns/Planck1/L0100N1504/PE/REFERENCE/data'
+tag = '027_z000p101'
+
+# sp.directory = directory
+# sp.tag = tag
+# sp.redshift = float(sp.tag[5:].replace('p','.'))
+# sp.scale_factor = 1.0 / (1+sp.redshift)
+
+p_sgrpn = E.readArray('PARTDATA', directory, tag, '/PartType4/SubGroupNumber')
+p_grpn = E.readArray('PARTDATA', directory, tag, '/PartType4/GroupNumber')
+p_imass = E.readArray('PARTDATA', directory, tag, '/PartType4/InitialMass') * 1e10
+p_form = E.readArray('PARTDATA', directory, tag, '/PartType4/StellarFormationTime')
+p_metal = E.readArray('PARTDATA', directory, tag, '/PartType4/SmoothedMetallicity')
+p_cood = E.readArray('PARTDATA', directory, tag, '/PartType4/Coordinates', physicalUnits=True)
+
+shgrpno = E.readArray('SUBFIND', directory, tag, '/Subhalo/SubGroupNumber')
+grpno = E.readArray('SUBFIND', directory, tag, '/Subhalo/GroupNumber')
+mstar_30 = E.readArray('SUBFIND', directory, tag, '/Subhalo/ApertureMeasurements/Mass/030kpc')[:,4] * 1e10
+mstar = E.readArray('SUBFIND', directory, tag, '/Subhalo/Stars/Mass') * 1e10
+sf_metal = E.readArray('SUBFIND', directory, tag, '/Subhalo/SF/Metallicity')
+sf_mass = E.readArray('SUBFIND', directory, tag, '/Subhalo/SF/Mass') * 1e10
+sfr_30 = E.readArray('SUBFIND', directory, tag, '/Subhalo/ApertureMeasurements/SFR/030kpc')
+sfr = E.readArray('SUBFIND', directory, tag, '/Subhalo/StarFormationRate')
+cop = E.readArray('SUBFIND', directory, tag, '/Subhalo/CentreOfPotential', physicalUnits=True)
+
+log_masses = np.log10(mstar)
+
+# indices = np.where((log_masses > 10.5) & (log_masses < 10.8))[0]
+indices = np.where(log_masses > 10.5)[0]
+
+# np.random.seed(seed=1)
+print("N: %s"%len(indices))
+
+
+with h5py.File('data/full_histories_eagle.h5','a') as f:
+
+    if "Star Particles" not in list(f.keys()): f.create_group("Star Particles")
+    if "Subhalos" not in list(f.keys()): f.create_group("Subhalos")
+
+    ## delete fields ##
+    for field in list(f["Subhalos"].keys()):
+        del f['Subhalos/%s'%field]
+          
+    for field in list(f["Star Particles"].keys()):
+        del f['Star Particles/%s'%field] 
+
+
+    # set redshift
+    f.attrs['redshift'] = float(tag[5:].replace('p','.'))
+    
+    
+
+        
+with h5py.File('data/full_histories_eagle.h5','a') as f:
+    
+    if "ID" not in list(f["Subhalos"].keys()):
+        f["Subhalos"].create_dataset('ID',(shape,),dtype='i8',maxshape=(None,),data=np.ones(shape)*-1)
+    
+    if "Stellar Mass" not in list(f["Subhalos"].keys()):
+        f["Subhalos"].create_dataset('Stellar Mass',(shape,),maxshape=(None,))
+        
+    if "Gas Metallicity" not in list(f["Subhalos"].keys()):
+        f["Subhalos"].create_dataset('Gas Metallicity',(shape,),maxshape=(None,))
+        
+    if "Stellar Metallicity" not in list(f["Subhalos"].keys()):
+        f["Subhalos"].create_dataset('Stellar Metallicity',(shape,),maxshape=(None,))
+        
+        f["Subhalos"].create_dataset('SFR',(shape,),maxshape=(None,))
+        
+    if "Star Forming Gas Mass" not in list(f["Subhalos"].keys()):
+        f['Subhalos'].create_dataset('Star Forming Gas Mass',(shape,),maxshape=(None,))
+        
+    if "Stellar Mass 30kpc" not in list(f["Subhalos"].keys()):
+        f['Subhalos'].create_dataset('Stellar Mass 30kpc',(shape,),maxshape=(None,))
+        
+    if "Stellar Metallicity 30kpc" not in list(f["Subhalos"].keys()):
+        f['Subhalos'].create_dataset('Stellar Metallicity 30kpc',(shape,),maxshape=(None,))
+        
+    if "Index Start" not in list(f["Subhalos"].keys()):
+        f["Subhalos"].create_dataset('Index Start',(shape,),dtype='i8',maxshape=(None,))
+        
+    if "Index Length" not in list(f["Subhalos"].keys()):
+        f["Subhalos"].create_dataset('Index Length',(shape,),dtype='i8',maxshape=(None,))
+    
+    if "Formation Time" not in list(f["Star Particles"].keys()):
+        f["Star Particles"].create_dataset('Formation Time',(0,),maxshape=(None,))
+        
+    if "Initial Mass" not in list(f["Star Particles"].keys()):
+        f["Star Particles"].create_dataset('Initial Mass',(0,),maxshape=(None,))
+        
+    if "Metallicity" not in list(f["Star Particles"].keys()):
+        f["Star Particles"].create_dataset('Metallicity',(0,),maxshape=(None,))
+        
+
+with h5py.File('data/full_histories_eagle.h5','a') as f:
+
+    pidx = len(f["Star Particles/Formation Time"][:])
+    print("Initial pidx:", pidx)
+
+    for key in indices:
+        print(key)
+
+        mask = (p_sgrpn == shgrpno[key]) & (p_grpn == grpno[key])
+        mask[mask] = cdist(p_cood[mask], np.expand_dims(cop[key], 0))[:,0] < 0.03
+
+        if sid in f["Subhalos/ID"][:]:
+            continue
+
+        if (i % 100) == 0: print(round(float(i)/len(subhalo_ids) * 100,2), "%")
+
+        #out = get(get_sub_url(sid, sim=sim['name'], snap=snap['number']))
+        #mstar, gas_metallicity, sfr, stellar_metallicity = [out[key] for key in \
+        #            ['mass_stars','gasmetallicitysfr','sfr','starmetallicity']]
+        #particles, star_forming_gas_mass, com = \
+        #    get_sub_particles(sid, sim=sim['name'], snap=snap['number'])
+        
+        f["Subhalos/ID"][i] = key
+        f["Subhalos/Stellar Mass"][i] = mstar[key]
+        f["Subhalos/Gas Metallicity"][i] = sf_metal[key]
+        # f["Subhalos/Stellar Metallicity"][i] = (stellar_metallicity * 1e10) / cosmo.h
+        f["Subhalos/SFR"][i] = sfr[key]
+        f["Subhalos/Star Forming Gas Mass"][i] = sf_mass[key]
+        f["Subhalos/Stellar Mass 30kpc"][i] = mstar_30[key] 
+        f["Subhalos/Stellar Metallicity 30kpc"][i] = np.mean(p_metal[mask])
+
+        plen = np.sum(mask) # len(particles['formationTime'])
+        print(pidx,plen,pidx+plen)
+        
+        f["Subhalos/Index Start"][i] = pidx
+        f["Subhalos/Index Length"][i] = plen
+        
+        dset = f["Star Particles/Formation Time"]
+        dset.resize(size=(pidx+plen,))
+        dset[pidx:] = p_form[mask]
+        
+        dset = f["Star Particles/Initial Mass"]
+        dset.resize(size=(pidx+plen,))
+        dset[pidx:] = p_imass[mask]
+        
+        dset = f["Star Particles/Metallicity"]
+        dset.resize(size=(pidx+plen,))
+        dset[pidx:] = p_metal[mask]
+        
+        pidx += plen
+        
+        f.flush()
+
+    
+# sp.filename = '/gpfs/data/dc-love2/full_histories_eagle_test.p'
+# sp.save()
