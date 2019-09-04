37143ce5afea297f9da35e73e301e14ad4881d90
christopherlovell <chrisl1991@hotmail.co.uk>
2019-09-04 14:24:16 +0100

    fixed noh

--- download_eagle_histories.py
+++ download_eagle_histories.py
@@ -1,46 +1,37 @@
-## Must be run on COSMA
-
-import eagle as E
+import eagle_IO.eagle_IO as E
 import numpy as np
-
+import h5py
 from scipy.spatial.distance import cdist
 
-# import sys
-# sys.path.append('/cosma5/data/dp004/dc-love2/sph2sed')
-# from sph2sed.model import sed
-# sp = sed()
 
 directory = '/cosma5/data/Eagle/ScienceRuns/Planck1/L0100N1504/PE/REFERENCE/data'
 tag = '027_z000p101'
 
-# sp.directory = directory
-# sp.tag = tag
-# sp.redshift = float(sp.tag[5:].replace('p','.'))
-# sp.scale_factor = 1.0 / (1+sp.redshift)
 
-p_sgrpn = E.readArray('PARTDATA', directory, tag, '/PartType4/SubGroupNumber')
-p_grpn = E.readArray('PARTDATA', directory, tag, '/PartType4/GroupNumber')
-p_imass = E.readArray('PARTDATA', directory, tag, '/PartType4/InitialMass') * 1e10
-p_form = E.readArray('PARTDATA', directory, tag, '/PartType4/StellarFormationTime')
-p_metal = E.readArray('PARTDATA', directory, tag, '/PartType4/SmoothedMetallicity')
-p_cood = E.readArray('PARTDATA', directory, tag, '/PartType4/Coordinates', physicalUnits=True)
+p_sgrpn = E.read_array('PARTDATA', directory, tag, '/PartType4/SubGroupNumber',noH=True, physicalUnits=False,numThreads=1)
+p_grpn = E.read_array('PARTDATA', directory, tag, '/PartType4/GroupNumber',noH=True, physicalUnits=False,numThreads=1)
+p_imass = E.read_array('PARTDATA', directory, tag, '/PartType4/InitialMass',noH=True, physicalUnits=False,numThreads=1) * 1e10
+p_form = E.read_array('PARTDATA', directory, tag, '/PartType4/StellarFormationTime',noH=True, physicalUnits=False,numThreads=1)
+p_metal = E.read_array('PARTDATA', directory, tag, '/PartType4/SmoothedMetallicity',noH=True, physicalUnits=False,numThreads=1)
+p_cood = E.read_array('PARTDATA', directory, tag, '/PartType4/Coordinates',noH=True, physicalUnits=False,numThreads=1)
 
-shgrpno = E.readArray('SUBFIND', directory, tag, '/Subhalo/SubGroupNumber')
-grpno = E.readArray('SUBFIND', directory, tag, '/Subhalo/GroupNumber')
-mstar_30 = E.readArray('SUBFIND', directory, tag, '/Subhalo/ApertureMeasurements/Mass/030kpc')[:,4] * 1e10
-mstar = E.readArray('SUBFIND', directory, tag, '/Subhalo/Stars/Mass') * 1e10
-sf_metal = E.readArray('SUBFIND', directory, tag, '/Subhalo/SF/Metallicity')
-sf_mass = E.readArray('SUBFIND', directory, tag, '/Subhalo/SF/Mass') * 1e10
-sfr_30 = E.readArray('SUBFIND', directory, tag, '/Subhalo/ApertureMeasurements/SFR/030kpc')
-sfr = E.readArray('SUBFIND', directory, tag, '/Subhalo/StarFormationRate')
-cop = E.readArray('SUBFIND', directory, tag, '/Subhalo/CentreOfPotential', physicalUnits=True)
+shgrpno = E.read_array('SUBFIND', directory, tag, '/Subhalo/SubGroupNumber',noH=True, physicalUnits=False,numThreads=1)
+grpno = E.read_array('SUBFIND', directory, tag, '/Subhalo/GroupNumber',noH=True, physicalUnits=False,numThreads=1)
+
+mstar_30 = E.read_array('SUBFIND', directory, tag, '/Subhalo/ApertureMeasurements/Mass/030kpc',noH=True, physicalUnits=False,numThreads=1)[:,4] * 1e10
+mstar = E.read_array('SUBFIND', directory, tag, '/Subhalo/Stars/Mass',noH=True, physicalUnits=False,numThreads=1) * 1e10
+
+stellar_metallicity = E.read_array('SUBFIND', directory, tag, '/Subhalo/Stars/Metallicity',noH=True, physicalUnits=False,numThreads=1)
+sf_metal = E.read_array('SUBFIND', directory, tag, '/Subhalo/SF/Metallicity',noH=True, physicalUnits=False,numThreads=1)
+sf_mass = E.read_array('SUBFIND', directory, tag, '/Subhalo/SF/Mass',noH=True, physicalUnits=False,numThreads=1) * 1e10
+sfr_30 = E.read_array('SUBFIND', directory, tag, '/Subhalo/ApertureMeasurements/SFR/030kpc',noH=True, physicalUnits=False,numThreads=1)
+sfr = E.read_array('SUBFIND', directory, tag, '/Subhalo/StarFormationRate',noH=True, physicalUnits=False,numThreads=1)
+cop = E.read_array('SUBFIND', directory, tag, '/Subhalo/CentreOfPotential',noH=True, physicalUnits=False,numThreads=1)
+
 
 log_masses = np.log10(mstar)
-
-# indices = np.where((log_masses > 10.5) & (log_masses < 10.8))[0]
-indices = np.where(log_masses > 10.5)[0]
-
-# np.random.seed(seed=1)
+indices = np.where(log_masses > 10.0)[0]
+shape = len(indices)
 print("N: %s"%len(indices))
 
 
@@ -57,12 +48,6 @@ with h5py.File('data/full_histories_eagle.h5','a') as f:
         del f['Star Particles/%s'%field] 
 
 
-    # set redshift
-    f.attrs['redshift'] = float(tag[5:].replace('p','.'))
-    
-    
-
-        
 with h5py.File('data/full_histories_eagle.h5','a') as f:
     
     if "ID" not in list(f["Subhalos"].keys()):
@@ -109,34 +94,27 @@ with h5py.File('data/full_histories_eagle.h5','a') as f:
     pidx = len(f["Star Particles/Formation Time"][:])
     print("Initial pidx:", pidx)
 
-    for key in indices:
-        print(key)
+    for i,key in enumerate(indices):
+        # print(key)
 
         mask = (p_sgrpn == shgrpno[key]) & (p_grpn == grpno[key])
         mask[mask] = cdist(p_cood[mask], np.expand_dims(cop[key], 0))[:,0] < 0.03
 
-        if sid in f["Subhalos/ID"][:]:
+        if key in f["Subhalos/ID"][:]:
             continue
 
-        if (i % 100) == 0: print(round(float(i)/len(subhalo_ids) * 100,2), "%")
+        if (i % 100) == 0: print(round(float(i)/shape * 100,2), "%")
 
-        #out = get(get_sub_url(sid, sim=sim['name'], snap=snap['number']))
-        #mstar, gas_metallicity, sfr, stellar_metallicity = [out[key] for key in \
-        #            ['mass_stars','gasmetallicitysfr','sfr','starmetallicity']]
-        #particles, star_forming_gas_mass, com = \
-        #    get_sub_particles(sid, sim=sim['name'], snap=snap['number'])
         
         f["Subhalos/ID"][i] = key
         f["Subhalos/Stellar Mass"][i] = mstar[key]
         f["Subhalos/Gas Metallicity"][i] = sf_metal[key]
-        # f["Subhalos/Stellar Metallicity"][i] = (stellar_metallicity * 1e10) / cosmo.h
         f["Subhalos/SFR"][i] = sfr[key]
         f["Subhalos/Star Forming Gas Mass"][i] = sf_mass[key]
         f["Subhalos/Stellar Mass 30kpc"][i] = mstar_30[key] 
-        f["Subhalos/Stellar Metallicity 30kpc"][i] = np.mean(p_metal[mask])
 
         plen = np.sum(mask) # len(particles['formationTime'])
-        print(pidx,plen,pidx+plen)
+        # print(pidx,plen,pidx+plen)
         
         f["Subhalos/Index Start"][i] = pidx
         f["Subhalos/Index Length"][i] = plen
@@ -158,5 +136,3 @@ with h5py.File('data/full_histories_eagle.h5','a') as f:
         f.flush()
 
     
-# sp.filename = '/gpfs/data/dc-love2/full_histories_eagle_test.p'
-# sp.save()
